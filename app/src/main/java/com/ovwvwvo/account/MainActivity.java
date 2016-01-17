798e5df5042e95899556b1d37c8fab1a17f6f72f
anye0803 <anye0803@163.com>
2016-01-17 16:36:51 +0800

    20160117

--- app/src/main/java/com/ovwvwvo/account/MainActivity.java
+++ app/src/main/java/com/ovwvwvo/account/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        System.out.println("hhh");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
